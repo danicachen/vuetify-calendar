fee9cf3cf84156b63d857ba9b16b012d34cc4cc3
DanicaChen <doyenwork@icloud.com>
2022-02-05 23:37:43 +0800

    data curd function

--- src/components/Calendar.vue
+++ src/components/Calendar.vue
@@ -2,140 +2,147 @@
   <v-row class="fill-height">
     <v-col>
       <v-sheet height="64">
-        <v-toolbar
-          flat
-        >
-          <v-btn
-            outlined
-            class="mr-4"
-            color="grey darken-2"
-            @click="setToday"
-          >
-            今天
+        <v-toolbar flat color="white">
+          <v-btn color="primary" dark @click.stop="dialog = true">
+            New Event
           </v-btn>
-          <v-btn
-            fab
-            text
-            small
-            color="grey darken-2"
-            @click="prev"
-          >
-            <v-icon small>
-              mdi-chevron-left
-            </v-icon>
+          <v-btn outlined class="mr-4" @click="setToday">
+            Today
           </v-btn>
-          <v-btn
-            fab
-            text
-            small
-            color="grey darken-2"
-            @click="next"
-          >
-            <v-icon small>
-              mdi-chevron-right
-            </v-icon>
+          <v-btn fab text small @click="prev">
+            <v-icon small>mdi-chevron-left</v-icon>
           </v-btn>
-          <v-toolbar-title v-if="$refs.calendar">
-            {{ $refs.calendar.title }}
-          </v-toolbar-title>
-          <v-spacer></v-spacer>
-          <v-menu
-            bottom
-            right
-          >
-            <template v-slot:activator="{ on, attrs }">
-              <v-btn
-                outlined
-                color="grey darken-2"
-                v-bind="attrs"
-                v-on="on"
-              >
+          <v-btn fab text small @click="next">
+            <v-icon small>mdi-chevron-right</v-icon>
+          </v-btn>
+          <v-toolbar-title>{{ title }}</v-toolbar-title>
+          <div class="flex-grow-1"></div>
+          <v-menu bottom right>
+            <template v-slot:activator="{ on }">
+              <v-btn outlined v-on="on">
                 <span>{{ typeToLabel[type] }}</span>
-                <v-icon right>
-                  mdi-menu-down
-                </v-icon>
+                <v-icon right>mdi-menu-down</v-icon>
               </v-btn>
             </template>
             <v-list>
               <v-list-item @click="type = 'day'">
-                <v-list-item-title>天</v-list-item-title>
+                <v-list-item-title>Day</v-list-item-title>
               </v-list-item>
               <v-list-item @click="type = 'week'">
-                <v-list-item-title>周</v-list-item-title>
+                <v-list-item-title>Week</v-list-item-title>
               </v-list-item>
               <v-list-item @click="type = 'month'">
-                <v-list-item-title>月</v-list-item-title>
+                <v-list-item-title>Month</v-list-item-title>
               </v-list-item>
               <v-list-item @click="type = '4day'">
-                <v-list-item-title>四天</v-list-item-title>
+                <v-list-item-title>4 days</v-list-item-title>
               </v-list-item>
             </v-list>
           </v-menu>
         </v-toolbar>
       </v-sheet>
-      <v-sheet height="600">
-        <v-calendar
-          ref="calendar"
-          v-model="focus"
-          color="primary"
-          :events="events"
-          :event-color="getEventColor"
-          :now="today"
-          :type="type"
-          @click:event="showEvent"
-          @click:more="viewDay"
-          @click:date="viewDay"
-          @change="updateRange"
-        ></v-calendar>
-        <v-menu
-          v-model="selectedOpen"
-          :close-on-content-click="false"
-          :activator="selectedElement"
-          offset-x
-        >
-          <v-card
-            color="grey lighten-4"
-            min-width="350px"
-            flat
-          >
-            <v-toolbar
-              :color="selectedEvent.color"
-              dark
-            >
-              <v-btn icon>
-                <v-icon>mdi-pencil</v-icon>
+
+      <v-dialog v-model="dialog" max-width="500">
+        <v-card>
+          <v-container>
+            <v-form @submit.prevent="addEvent">
+              <v-text-field v-model="name" type="text" label="event name (required)"></v-text-field>
+              <v-text-field v-model="details" type="text" label="detail"></v-text-field>
+              <v-text-field v-model="start" type="date" label="start (required)"></v-text-field>
+              <v-text-field v-model="end" type="date" label="end (required)"></v-text-field>
+              <v-text-field v-model="color" type="color" label="color (click to open color menu)"></v-text-field>
+              <v-btn type="submit" color="primary" class="mr-4" @click.stop="dialog = false">
+                create event
               </v-btn>
-              <v-toolbar-title v-html="selectedEvent.name"></v-toolbar-title>
-              <v-spacer></v-spacer>
-              <v-btn icon>
-                <v-icon>mdi-heart</v-icon>
+            </v-form>
+          </v-container>
+        </v-card>
+      </v-dialog>
+
+      <v-dialog v-model="dialogDate" max-width="500">
+        <v-card>
+          <v-container>
+            <v-form @submit.prevent="addEvent">
+              <v-text-field v-model="name" type="text" label="event name (required)"></v-text-field>
+              <v-text-field v-model="details" type="text" label="detail"></v-text-field>
+              <v-text-field v-model="start" type="date" label="start (required)"></v-text-field>
+              <v-text-field v-model="end" type="date" label="end (required)"></v-text-field>
+              <v-text-field v-model="color" type="color" label="color (click to open color menu)"></v-text-field>
+              <v-btn type="submit" color="primary" class="mr-4" @click.stop="dialog = false">
+                create event
               </v-btn>
-              <v-btn icon>
-                <v-icon>mdi-dots-vertical</v-icon>
-              </v-btn>
-            </v-toolbar>
-            <v-card-text>
-              <span v-html="selectedEvent.details"></span>
-            </v-card-text>
-            <v-card-actions>
-              <v-btn
-                text
-                color="secondary"
-                @click="selectedOpen = false"
-              >
-                Cancel
-              </v-btn>
-            </v-card-actions>
-          </v-card>
-        </v-menu>
-      </v-sheet>
-    </v-col>
-  </v-row>
+            </v-form>
+          </v-container>
+        </v-card>
+      </v-dialog>
+
+<v-sheet height="600">
+  <v-calendar
+  ref="calendar"
+  v-model="focus"
+  color="primary"
+  :events="events"
+  :event-color="getEventColor"
+  :event-margin-bottom="3"
+  :now="today"
+  :type="type"
+  @click:event="showEvent"
+  @click:more="viewDay"
+  @click:date="setDialogDate"
+  @change="updateRange"
+  ></v-calendar>
+  <v-menu
+  v-model="selectedOpen"
+  :close-on-content-click="false"
+  :activator="selectedElement"
+  full-width
+  offset-x
+  >
+  <v-card color="grey lighten-4" :width="350" flat>
+    <v-toolbar :color="selectedEvent.color" dark>
+      <v-btn @click="deleteEvent(selectedEvent.id)" icon>
+        <v-icon>mdi-delete</v-icon>
+      </v-btn>
+      <v-toolbar-title v-html="selectedEvent.name"></v-toolbar-title>
+      <div class="flex-grow-1"></div>
+    </v-toolbar>
+
+    <v-card-text>
+      <form v-if="currentlyEditing !== selectedEvent.id">
+        {{ selectedEvent.details }}
+      </form>
+      <form v-else>
+        <textarea-autosize
+        v-model="selectedEvent.details"
+        type="text"
+        style="width: 100%"
+        :min-height="100"
+        placeholder="add note">
+      </textarea-autosize>
+    </form>
+  </v-card-text>
+
+  <v-card-actions>
+    <v-btn text color="secondary" @click="selectedOpen = false">
+      close
+    </v-btn>
+    <v-btn v-if="currentlyEditing !== selectedEvent.id" text @click.prevent="editEvent(selectedEvent)">
+      edit
+    </v-btn>
+    <v-btn text v-else type="submit" @click.prevent="updateEvent(selectedEvent)">
+      Save
+    </v-btn>
+  </v-card-actions>
+</v-card>
+</v-menu>
+</v-sheet>
+</v-col>
+</v-row>
 </template>
 
 <script>
 import db from '@/firebase/firebaseinit'
-import { collection, getDocs } from "firebase/firestore";
+import { collection, getDocs,doc, deleteDoc,addDoc,updateDoc } from "firebase/firestore";
 export default {
   data: () => ({
     today: new Date().toISOString().substr(0, 10),
@@ -228,7 +235,7 @@ export default {
     },
     async addEvent () {
       if (this.name && this.start && this.end) {
-        await db.collection("calEvent").add({
+        await addDoc(collection(db,"calEvent"),{
           name: this.name,
           details: this.details,
           start: this.start,
@@ -249,14 +256,15 @@ export default {
       this.currentlyEditing = ev.id
     },
     async updateEvent (ev) {
-      await db.collection('calEvent').doc(this.currentlyEditing).update({
+      const newDetail = doc(db,"id",this.currentlyEditing)
+      await updateDoc(newDetail,{
         details: ev.details
       })
       this.selectedOpen = false,
       this.currentlyEditing = null
     },
     async deleteEvent (ev) {
-      await db.collection("calEvent").doc(ev).delete()
+      await deleteDoc(doc(db,"calEvent",ev))
       this.selectedOpen = false,
       this.getEvents()
     },
