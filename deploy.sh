26c7c50d798999500339fed290229abee58cfc88
DanicaChen <doyenwork@icloud.com>
2022-02-05 15:40:17 +0800

    edit config file

--- deploy.sh
+++ deploy.sh
@@ -16,11 +16,4 @@ git init
 git add -A
 git commit -m 'deploy'
 
-# 部署到 https://<USERNAME>.github.io
-# git push -f git@github.com:<USERNAME>/<USERNAME>.github.io.git master
-
-# 部署到 https://<USERNAME>.github.io/<REPO>
-git push -f git@github.com:danicachen
-/vuetify-calendar.git main:gh-pages
-
 cd -
